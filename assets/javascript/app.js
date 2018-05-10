7601f5cc5d096f05c96abe70f1d403b816843601
cronk <ChristineRonk@Stevens-MacBook-Pro.local>
2018-05-09 21:09:41 -0500

    added styling and fixed bug where second set of gifs wouldn't animate

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,41 +4,30 @@ $(document).ready(function(){
 // This is the array that holds our topics that will be displayed as buttons
 var topics = ["Doc", "Grumpy", "Happy", "Sleepy", "Dopey", "Bashful", "Sneezy", "Snow White"];
 // functions
-    
-function renderButtons(){
-    // this prevents repeat buttons when we run the renderButtons function
-    $("#buttons-view").empty();
-    // loop through array and create buttons
-    for(var i = 0; i < topics.length; i++) {
-        //  buttons
-        var a = $("<button>");
-        a.addClass("topic-btn");
-        a.attr("data-name", topics[i]);
-        a.text(topics[i]);
-        $("#buttons-view").append(a);
-    } 
-}
+  
 
 // updating HTML
 function getGiphy() {
-    $("#buttons-view").empty();
+
+    // $("#buttons-view").empty();
     var topic = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+topic+"&api_key=VMsRLkRqF0tK9SLZfaP35fhJld2jmivR&limit=10";
+    // queryURL with searches rating will be PG or less (as I got some crazy ones) and limit-10 displays 10 gifs
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+topic+"&rating=PG&api_key=VMsRLkRqF0tK9SLZfaP35fhJld2jmivR&limit=10";
     
     // AJAX call for button being clicked
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        
-        for(var i = 0; i < topics.length; i++){
 
+        for(var i = 0; i < response.data.length; i++){
+            $("#gifs").empty();
             // creates div for gifs
             var gifsDiv = $("<div class = 'gifs'>");
 
             // holds ratings
             var rating = response.data[i].rating;
-            var ratings = $("<p>").text("Rating: " + rating);
+            var ratings = $("<p>").text("Rating: " + rating.toUpperCase());
 
             // giphy images 
             var giphyImages = $("<img>");
@@ -53,33 +42,53 @@ function getGiphy() {
             gifsDiv.prepend(giphyImages);
             gifsDiv.prepend(ratings);
             $("#gifsGoHere").prepend(gifsDiv);
-       
         }
     });
+}
 
-    renderButtons();
+
+// this works
+function renderButtons() {
+    // this prevents repeat buttons when we run the renderButtons function
+    $("#buttons-view").empty();
+    $(".gifs").empty();
+
+    // loop through array and create buttons
+    for(var i = 0; i < topics.length; i++) {
+        //  buttons
+        var btns = $("<button>");
+        btns.addClass("topic-btn");
+        btns.attr("data-name", topics[i]);
+        btns.text(topics[i]);
+        $("#buttons-view").append(btns);
+    } 
+}
 
     $("#add-topic").on("click", function(event) {
         event.preventDefault();
+
         // .trim prevents blank spaces beforehand
         var addTopic = $("#user-input").val().trim();
         topics.push(addTopic);
+        console.log(addTopic);
         renderButtons();
+        // clears user field after we've collected another topic
+        $("#user-input").val(" ");
     });
 
     $("body").on("click", ".giphyImage", function(){
-        event.preventDefault();
+        event.preventDefault();            
         var state = $(this).attr("data-state");
 
         if (state === "still") {
             $(this).attr("src", $(this).attr("data-animate"));
             $(this).attr("data-state", "animate");
+
         } else {
             $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-state", "still");
         }
     })
-}
 
 // click event listener to all with class of "topic-btn"
 $(document).on("click", ".topic-btn", getGiphy);
